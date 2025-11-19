0d0496ef71b54f39bb7dcbc58f309a7eb4a5f58b
Angga Saputra <anggasaputra2st@gmail.com>
2025-11-19 20:43:40 +0800

    add routes: login, register, password-request, login-function

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name("home");
+Route::get('/login', function () {
+    return view('auth.login');
+})->name("login");
+Route::get('/password-request', function () {
+    return view('auth.login');
+})->name("password.request");
+Route::get('/register', function () {
+    return view('auth.register');
+})->name("register");
+
+Route::post('/login', function () {
+    return view('auth.login');
+})->name("login");
